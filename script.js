604188f471945d481b92ea991db08b946d0ae2df
Ajay KC <meajay64@gmail.com>
2022-07-29 20:07:28 +0900

    Bind html elem inside object

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
-let home = document.querySelector("#home");
-let books = document.querySelector("#books");
-let container = document.querySelector(".container");
-let main = document.querySelector("main");
-let addBookBtn = document.querySelector(".js-add-btn");
-let submitBtn = document.querySelector(".btn-submit");
-let formContainer = document.querySelector(".form-container");
-let form = document.querySelector("form");
-let updateForm = document.querySelector(".update-form");
-let updateFormContainer = document.querySelector(".update-form-container");
-let introContainer = document.querySelector(".intro");
+let elem = (function () {
+  let home = document.querySelector("#home");
+  let books = document.querySelector("#books");
+  let container = document.querySelector(".container");
+  let main = document.querySelector("main");
+  let addBookBtn = document.querySelector(".js-add-btn");
+  let submitBtn = document.querySelector(".btn-submit");
+  let formContainer = document.querySelector(".form-container");
+  let form = document.querySelector("form");
+  let updateForm = document.querySelector(".update-form");
+  let updateFormContainer = document.querySelector(".update-form-container");
+  let introContainer = document.querySelector(".intro");
+
+  return {
+    home,
+    books,
+    container,
+    main,
+    addBookBtn,
+    submitBtn,
+    formContainer,
+    form,
+    updateForm,
+    updateFormContainer,
+    introContainer,
+  };
+})();
+
 let editObjIndex;
 let myLibraryLocalStorage = JSON.parse(localStorage.getItem("myLibrary"));
 let myLibrary = myLibraryLocalStorage || [];
 
 addBookToView();
 
-addBookBtn.addEventListener("click", () => {
-  formContainer.classList.remove("hide");
-  main.classList.add("hide");
-  introContainer.classList.add("hide");
-  // form.classList.remove('hide');
+elem.addBookBtn.addEventListener("click", () => {
+  elem.formContainer.classList.remove("hide");
+  elem.main.classList.add("hide");
+  elem.introContainer.classList.add("hide");
 });
 
-form.addEventListener("submit", (e) => {
+elem.form.addEventListener("submit", (e) => {
   let title = document.querySelector("#title").value;
   let author = document.querySelector("#author").value;
   let totalPages = document.querySelector("#total_pages").value;
@@ -31,17 +47,17 @@ form.addEventListener("submit", (e) => {
   let bookObj = new Book(title, author, totalPages, pageYouOn);
   myLibrary.push(bookObj);
   localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-  main.classList.remove("hide");
-  formContainer.classList.add("hide");
+  elem.main.classList.remove("hide");
+  elem.formContainer.classList.add("hide");
 
   clearView();
   addBookToView();
-  form.reset();
+  elem.form.reset();
   e.preventDefault();
 });
 
 // || Event for form update...
-updateForm.addEventListener("submit", (e) => {
+elem.updateForm.addEventListener("submit", (e) => {
   let title = document.querySelector("#title_update").value;
   let author = document.querySelector("#author_update").value;
   let totalPages = document.querySelector("#total_pages_update").value;
@@ -51,27 +67,27 @@ updateForm.addEventListener("submit", (e) => {
   myLibrary.splice(editObjIndex, 1, book);
   localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
 
-  main.classList.remove("hide");
-  updateFormContainer.classList.add("hide");
+  elem.main.classList.remove("hide");
+  elem.updateFormContainer.classList.add("hide");
   clearView();
   addBookToView();
-  updateForm.reset();
+  elem.updateForm.reset();
   e.preventDefault();
 });
 
 // Click event for 'home' and 'books' link
-home.addEventListener("click", () => {
-  introContainer.classList.remove("hide");
-  main.classList.add("hide");
-  formContainer.classList.add("hide");
-  updateFormContainer.classList.add("hide");
+elem.home.addEventListener("click", () => {
+  elem.introContainer.classList.remove("hide");
+  elem.main.classList.add("hide");
+  elem.formContainer.classList.add("hide");
+  elem.updateFormContainer.classList.add("hide");
 });
 
-books.addEventListener("click", () => {
-  introContainer.classList.add("hide");
-  main.classList.remove("hide");
-  formContainer.classList.add("hide");
-  updateFormContainer.classList.add("hide");
+elem.books.addEventListener("click", () => {
+  elem.introContainer.classList.add("hide");
+  elem.main.classList.remove("hide");
+  elem.formContainer.classList.add("hide");
+  elem.updateFormContainer.classList.add("hide");
 });
 
 //Function Declarations...
@@ -83,6 +99,8 @@ function Book(title, author, totalPages, pageYouOn) {
   this["page You On"] = pageYouOn;
 }
 
+
+
 // Add books to DOM
 function addBookToView() {
   myLibrary.forEach((obj, index) => {
@@ -91,7 +109,7 @@ function addBookToView() {
     let editBtn = document.createElement("button");
     let deleteBtn = document.createElement("button");
     box.classList.add("box");
-    container.appendChild(box);
+    elem.container.appendChild(box);
     for (let prop in obj) {
       let para = document.createElement("p");
       let strong = document.createElement("strong");
@@ -118,15 +136,15 @@ function addBookToView() {
     });
 
     editBtn.addEventListener("click", () => {
-      updateFormContainer.classList.remove("hide");
+      elem.updateFormContainer.classList.remove("hide");
 
-      updateForm.querySelector("#title_update").value = obj.title;
-      updateForm.querySelector("#author_update").value = obj.author;
-      updateForm.querySelector("#total_pages_update").value =
+      elem.updateForm.querySelector("#title_update").value = obj.title;
+      elem.updateForm.querySelector("#author_update").value = obj.author;
+      elem.updateForm.querySelector("#total_pages_update").value =
         obj["total pages"];
-      updateForm.querySelector("#page_you_on_update").value =
+      elem.updateForm.querySelector("#page_you_on_update").value =
         obj["page You On"];
-      main.classList.add("hide");
+      elem.main.classList.add("hide");
       editObjIndex = index;
     });
 
@@ -147,6 +165,10 @@ function clearView() {
 
   Array.from(boxes).forEach((item) => {
     let box = document.querySelector(".box");
-    container.removeChild(box);
+    elem.container.removeChild(box);
   });
 }
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("refreshed");
+});
